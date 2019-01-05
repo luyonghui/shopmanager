db177321dfc36f8292c0391d9ca523648c72c59c
ltm <ltm@alan.com>
2019-01-05 09:23:27 +0800

    home完成-退出-验证

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,145 @@
 <template>
-  <div>Home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">
+            <img src="@/assets/logo.png" alt="图片无法显示">
+          </div>
+        </el-col>
+        <el-col :span="19" class="middle">
+          <div class="grid-content bg-purple-light">
+            <h2>电商后台管理系统</h2>
+          </div>
+        </el-col>
+        <el-col :span="1" class="loginout">
+          <a href="#" @click.prevent="handleLoginout()">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu unique-opened default-active="2">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-3">
+              <i class></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-3">
+              <i class></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="1-4">
+              <i class></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-3">
+              <i class></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="1-4">
+              <i class></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="1-5">
+              <i class></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-3">
+              <i class></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-3">
+              <i class></i>
+              数据列表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+  // 判断页面上是否有token 如果有 就渲染页面，没有就回到登陆页面
+  beforeCreate() {
+    if (!localStorage.getItem("token")) {
+      this.$message.warning("请先登录");
+      this.$router.push({
+        name: "login"
+      });
+    }
+    // 之后会继续执行钩子函数  mounted(){}
+  },
+  methods: {
+    // 用户退出 清除token
+    handleLoginout() {
+      //  1 清除token  localStorage有自带的方法 clear  可以清除token
+      localStorage.clear();
+      //  2 回到登陆组件
+      this.$router.push({
+        name: "login"
+      });
+      //  3 提示
+      this.$message.success("退出成功");
+    }
+  }
+};
 </script>
 
 <style>
-
+.container {
+}
+.container .header {
+  background-color: #b3c0d1;
+}
+.container .aside {
+}
+.container .main {
+}
+.middle {
+  text-align: center;
+  color: #fff;
+  line-height: 60px;
+}
+.loginout a {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -23,44 +23,30 @@ export default {
     };
   },
   methods: {
-    handleLogin() {
-      this.$http.post("login", this.formdata).then(res => {
-        //  console.log(res);
-        //  const per = {data:{name:'abc'}}
-        //  const {data} = per
-        //  将per中data的数据赋值给data常量
-        // const {data:newdata} = per
-        // 将per中data的值 name:'abc' 赋值给newdata这个常量
-        // 这个写法 称之为  对象结构起别名的写法
-        //  ES6对象的结构赋值
-
-        const {
-          data: {
-            meta: { msg, status }
-          }
-        } = res;
-        if (status == 200) {
-          //  alert(msg)
-          // this.$message({
-          //   showClose: true,
-          //   message: msg,
-          //   type: "success"
-          // });
-          // 两种写法
-          this.$message.success(msg);
-          // 登录成功后渲染home.vue组件  改标识 编程式导航
-            this.$router.push({
-              name : "home"
-            })
-        } else {
-          this.$message.warning(msg);
-          // this.$message({
-          //   showClose: true,
-          //   message: msg,
-          //   type: "warning"
-          // });
+    async handleLogin() {
+      // axios请求是异步操作的请求  把异步的代码看起来像同步，需要用到ES7的语法 ： async + await  使用这两个组合
+      // 使用 ： 找到异步操作的代码 前面加 await
+      //         找到距离异步操作最近的函数 前面加 async
+    //  目的 ：   只是为了代码看起来舒服
+      const res = await this.$http.post("login", this.formdata);
+      // console.log(res);
+      const {
+        data: {
+          meta: { msg, status },
+          data
         }
-      });
+      } = res; 
+      if (status == 200) {
+        // 保存用户的token值(token值将来要使用
+        // console.log(res)
+        // localStorage.setItem() 当设置这个方法之后，在本地就会存储对象，此处是在本地存储了一个token
+        localStorage.setItem("token",data.token)
+        this.$router.push({
+          name: "home"
+        });
+      } else {
+        this.$message.warning(msg);
+      }
     }
   },
   mounted() {
@@ -88,3 +74,42 @@ export default {
   width: 100%;
 }
 </style>
+
+
+
+// this.$http.post("login", this.formdata).then(res => {
+//         //  console.log(res);
+//         //  const per = {data:{name:'abc'}}
+//         //  const {data} = per
+//         //  将per中data的数据赋值给data常量
+//         // const {data:newdata} = per
+//         // 将per中data的值 name:'abc' 赋值给newdata这个常量
+//         // 这个写法 称之为  对象结构起别名的写法
+//         //  ES6对象的结构赋值
+//         const {
+//           data: {
+//             meta: { msg, status }
+//           }
+//         } = res;
+//         if (status == 200) {
+//           //  alert(msg)
+//           // this.$message({
+//           //   showClose: true,
+//           //   message: msg,
+//           //   type: "success"
+//           // });
+//           // 两种写法
+//           this.$message.success(msg);
+//           // 登录成功后渲染home.vue组件  改标识 编程式导航
+//             this.$router.push({
+//               name : "home"
+//             })
+//         } else {
+//           this.$message.warning(msg);
+//           // this.$message({
+//           //   showClose: true,
+//           //   message: msg,
+//           //   type: "warning"
+//           // });
+//         }
+//       });
